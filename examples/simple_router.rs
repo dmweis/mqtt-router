ec096ca55358d268b11b393d95f831f44d81a8c7
David Weis <davidmichaelweis@gmail.com>
2022-06-24 20:31:16 +0100

    Add examples

---
+++ examples/simple_router.rs
@@ -0,0 +1,31 @@
+use mqtt_router::{async_trait, RouteHandler, Router, RouterError};
+
+pub struct ExampleHandler {}
+
+impl ExampleHandler {
+    pub fn new() -> Box<Self> {
+        Box::new(Self {})
+    }
+}
+
+#[async_trait]
+impl RouteHandler for ExampleHandler {
+    async fn call(&mut self, topic: &str, _content: &[u8]) -> Result<(), RouterError> {
+        println!("Handling mqtt message for {topic}");
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let mut router = Router::default();
+    router
+        .add_handler("home/test", ExampleHandler::new())
+        .unwrap();
+
+    for topic in router.topics_for_subscription() {
+        println!("{topic}");
+    }
+
+    router.handle_message("home/test", &[0]).await.unwrap();
+}
